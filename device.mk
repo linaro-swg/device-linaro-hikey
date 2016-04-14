d751acc4c57fcc1eb2c709a480fac12857d05d47
David Brown <david.brown@linaro.org>
2016-04-14 11:56:37 -0600

    Add optee_test and TA
    Change-Id: Ie2b23f217ca602ede4bba8c211647e9facffdc7c

--- device.mk
+++ device.mk
@@ -68,11 +68,20 @@ PRODUCT_PACKAGES += gralloc.hikey
 
 PRODUCT_PACKAGES += libteec \
 					tee-supplicant \
-					tee_helloworld
+					tee_helloworld \
+					xtest
 
 # OP TEE Hello world TA
 PRODUCT_PACKAGES += 8aaaf200-2450-11e4-abe20002a5d5c51b.ta
-
+PRODUCT_PACKAGES += e13010e0-2ae1-11e5-896a0002a5d5c51b.ta
+PRODUCT_PACKAGES += 5ce0c432-0ab0-40e5-a056782ca0e6aba2.ta
+PRODUCT_PACKAGES += c3f6e2c0-3548-11e1-b86c0800200c9a66.ta
+PRODUCT_PACKAGES += cb3e5ba0-adf1-11e0-998b0002a5d5c51b.ta
+PRODUCT_PACKAGES += 5b9e0e40-2636-11e1-ad9e0002a5d5c51b.ta
+PRODUCT_PACKAGES += d17f73a0-36ef-11e1-984a0002a5d5c51b.ta
+PRODUCT_PACKAGES += e6a33ed4-562b-463a-bb7eff5e15a493c8.ta
+PRODUCT_PACKAGES += b689f2a7-8adf-477a-9f9932e90c0ad0a2.ta
+PRODUCT_PACKAGES += f157cda0-550c-11e5-a6fa0002a5d5c51b.ta
 
 # Set zygote config
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
