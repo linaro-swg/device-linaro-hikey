d013ca8434367e9f11605e9f8e3c88eb39a44b86
John Stultz <john.stultz@linaro.org>
2018-04-11 12:17:34 -0700

    hikey/hikey960: Add more services for treble
    In preparation for the transition to FULL_TREBLE,
    add some missing services to the product packages.
    
    Credit for this patch goes to Vishal Bhoj <vishal.bhoj@linaro.org>
    who basically was the original author and magician
    who got things working.
    
    I've only refactored and split up his changes to help
    with the submissions.
    
    Change-Id: I8adb8f0c1f76bfdda6efd85446ba6daf7ce58fcc
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -41,6 +41,7 @@ PRODUCT_PACKAGES += audio.a2dp.default \
 		    tinyplay
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
@@ -54,7 +55,9 @@ PRODUCT_PACKAGES += libGLES_android
 # Graphics HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl
 
 # Memtrack
@@ -77,7 +80,9 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 
 # Sensor HAL
 ifneq ($(TARGET_SENSOR_MEZZANINE),)
