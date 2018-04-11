98e17f6e55e1f000b9be2df85e647ada33e741c1
John Stultz <john.stultz@linaro.org>
2018-04-11 15:33:22 -0700

    hikey/hikey960: Add vndk-sp libraries in preparation for treble
    Add logic to add vndk-sp libraries in order to
    get ready to switch to FULL_TREBLE.
    
    Credit for this patch goes to Vishal Bhoj <vishal.bhoj@linaro.org>
    who basically was the original author and magician
    who got things working.
    
    I've only refactored and split up his changes to help
    with the submissions.
    
    Change-Id: I99a5f75f7589f0fb591a5f8b4c2b982b2dc1e300
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -178,3 +178,35 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         device/linaro/hikey/etc/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
         frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp\
+    android.hardware.graphics.composer@2.1.vndk-sp\
+    android.hardware.graphics.allocator@2.0.vndk-sp\
+    android.hardware.graphics.mapper@2.0.vndk-sp\
+    android.hardware.graphics.common@1.0.vndk-sp\
+    libvndksupport.vndk-sp\
+    libbinder.vndk-sp\
+    libhwbinder.vndk-sp\
+    libbase.vndk-sp\
+    libfmq.vndk-sp\
+    libcutils.vndk-sp\
+    libhardware.vndk-sp\
+    libhidlbase.vndk-sp\
+    libhidltransport.vndk-sp\
+    libui.vndk-sp\
+    libutils.vndk-sp\
+    libc++.vndk-sp\
+    libRS_internal.vndk-sp\
+    libRSDriver.vndk-sp\
+    libRSCpuRef.vndk-sp\
+    libbcinfo.vndk-sp\
+    libblas.vndk-sp\
+    libft2.vndk-sp\
+    libpng.vndk-sp\
+    libcompiler_rt.vndk-sp\
+    libbacktrace.vndk-sp\
+    libunwind.vndk-sp\
+    libunwindstack.vndk-sp\
+    liblzma.vndk-sp\
+    libion.vndk-sp\
